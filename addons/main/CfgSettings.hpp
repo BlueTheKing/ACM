b7e96d63b2fec4dbaaaa0282c3b341c112a87be6
Blue <itzblueman@gmail.com>
2024-10-17 15:46:37 +0200

    Required version

---
+++ addons/main/CfgSettings.hpp
@@ -0,0 +1,12 @@
+class CfgSettings {
+    class CBA {
+        class Versioning {
+            class PREFIX {
+                class Dependencies {
+                    CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "(true)"};
+                    ACE[] = {"ace_main", REQUIRED_ACE_VERSION, "(true)"};
+                };
+            };
+        };
+    };
+};
