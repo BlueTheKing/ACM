7ebba5945838d944bf79c336cbf399ed1468e51a
Blue <itzblueman@gmail.com>
2025-03-27 08:59:17 +0100

    Lower chemical agent absorption rate

--- addons/cbrn/ACM_CBRN_Hazards.hpp
+++ addons/cbrn/ACM_CBRN_Hazards.hpp
@@ -15,18 +15,18 @@ class ACM_CBRN_Hazards {
             thresholds[] = {0,25,50};
             threshold_positiveRate[] = {1,1,1};
             threshold_negativeRate[] = {1,0.5,0.3};
-            absorption_rate = 0.9;
-            inhalation_rate = 1.2;
+            absorption_rate = 0.35;
+            inhalation_rate = 0.8;
             elimination_rate = -0.25;
             thresholdFunction = QFUNC(effectChlorine);
         };
 
         class Sarin {
             thresholds[] = {0,25,50,75};
-            threshold_positiveRate[] = {1,1,1,1};
+            threshold_positiveRate[] = {1,1,1,0.5};
             threshold_negativeRate[] = {1,0.5,0.25,0.1};
-            absorption_rate = 1;
-            inhalation_rate = 2;
+            absorption_rate = 0.5;
+            inhalation_rate = 1;
             elimination_rate = -0.25;
             thresholdFunction = QFUNC(effectSarin);
         };
@@ -35,8 +35,8 @@ class ACM_CBRN_Hazards {
             thresholds[] = {0,25};
             threshold_positiveRate[] = {1,1};
             threshold_negativeRate[] = {1,0.5};
-            absorption_rate = 1.5;
-            inhalation_rate = 1;
+            absorption_rate = 0.9;
+            inhalation_rate = 0.7;
             elimination_rate = -0.25;
             thresholdFunction = QFUNC(effectLewisite);
         };
